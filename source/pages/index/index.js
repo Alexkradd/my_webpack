5f75dfcc07abbde866b07250fa95c25bcc6500c6
Daria Rezanova <alexkradd@gmail.com>
2017-12-16 18:17:41 +0300

    webpack

---
+++ source/pages/index/index.js
@@ -0,0 +1,9 @@
+import 'normalize.css';
+import './index.scss';
+
+import menuMacker from '../../components/menu/menu';
+let menu = menuMacker(['Главная','Обо мне','Блог'], 'menu');
+document.body.appendChild(menu);
+
+console.log('in index.js');
+console.log($);
